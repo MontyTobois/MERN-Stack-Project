8b48b3fe1c8fc71af7b40db890260a529de9cec2
Andre-Tobois <tobois.andre@gmail.com>
2018-12-09 14:39:43 -0500

    feat: Can get items from database.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import AppNavBar from './components/AppNavBar';
 import ShoppingList from './components/ShoppingList';
 import './App.css';
@@ -7,10 +11,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+    <Provider store={store}>
       <div className="App">
       <AppNavBar />
       <ShoppingList />
       </div>
+    </Provider>
     );
   }
 }
